f073f7b55212ab4297608320cf6422c570f9877c
reyhanturhan <reyhan.turhan@sabanciuniv.edu>
2025-03-28 16:10:41 +0300

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { authController } = require('../controllers/authController'); // Import the auth controller
+const cartRoutes = require('./cartRoutes');
+router.use('/cart', cartRoutes);
 
 // Use a consistent naming convention under /auth
 // router.post('/auth/register', authController.register);
